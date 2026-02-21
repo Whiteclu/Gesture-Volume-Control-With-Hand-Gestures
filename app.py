bed52a350c0eff9239fe1977f43a630f4f01ae6a
BoomeshKS <boomeshalone@gmail.com>
2026-02-21 19:34:44 +0530

    code edited

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 import cv2
 import mediapipe as mp
 
-from mediapipe.python.solutions import hands
-from mediapipe.python.solutions import drawing_utils
+mp_hands = mp.solution.hands
 
-mp_hands = hands
 hands = mp_hands.Hands(
-    static_image_mode=False,
+    static_image_mode = False,
     max_num_hands=2,
     min_detection_confidence=0.6,
-    min_tracking_confidence=0.6
+    min_tracking_confidence=0.6,
 )
 
-mp_draw = drawing_utils
+mp_draw = mp.solution.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, frame = cap.read()
-    if not success:
-        break
+    success , frame = cap.read()
+    if not success: break
 
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -27,22 +24,21 @@ while True:
 
     if result.multi_hand_landmarks:
         for hand_landmarks in result.multi_hand_landmarks:
-
             mp_draw.draw_landmarks(
                 frame,
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS
             )
-
-            for id, lm in enumerate(hand_landmarks.landmark):
-                h, w, _ = frame.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                print(f"Landmark {id}: x={cx}, y={cy}")
-
+            for id, lm in enumerate(hand_landmarks.landmarks):
+                h,w,_ = frame.shape
+                cx,cy = int(lm.x*w), int(lm.y * h)
+                print(f"Landmark {id}: x={cx},y={cy}")
     cv2.imshow("Hand Detection", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & ord('q') == 0xFF:
         break
-
 cap.release()
 cv2.destroyAllWindows()
+
+            
+
+
